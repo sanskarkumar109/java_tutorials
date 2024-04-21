884b1e744ba2a70c1b019105f835079107467d07
Sanskar Kumar <133953471+sanskarkumar109@users.noreply.github.com>
2024-04-21 15:53:38 +0530

    Add files via upload

---
+++ array_2d.java
@@ -0,0 +1,19 @@
+public class array_2d {
+    public static void main(String[] args) {
+        int m=2,n=2;
+        int arr[][]=new int[m][n];
+        arr[0][0]=10;
+        arr[0][1]=20;
+        arr[1][0]=30;
+        arr[1][1]=40;
+
+        for(int i=0;i<=m-1;i++){
+            for(int j=0;j<=n-1;j++){
+                System.out.println(arr[m][n]);
+
+        
+    }
+        }
+
+    }
+}
