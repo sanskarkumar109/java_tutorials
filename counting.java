419f3f7e3e8ca65cbc08a29ceb180defcfedefc0
Sanskar Kumar <133953471+sanskarkumar109@users.noreply.github.com>
2024-04-25 19:50:32 +0530

    Add files via upload

---
+++ counting.java
@@ -0,0 +1,7 @@
+public class counting {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            System.out.println("counting from 1 to 10 is : "+i);
+        }
+    }
+}
