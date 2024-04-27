6dc0549ab3257427f9936a466f425c3fda04d9f0
Sanskar Kumar <133953471+sanskarkumar109@users.noreply.github.com>
2024-04-27 23:30:15 +0530

    Add files via upload

---
+++ date.java
@@ -0,0 +1,7 @@
+public class date {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        System.out.println("the maximum value of long is "+Long.MAX_VALUE);
+        System.out.println("the minimum value of long is "+Long.MIN_VALUE);
+    }
+}
