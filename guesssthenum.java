7b796dc45efc46b07c408a65800727fb6b3301c4
Sanskar Kumar <133953471+sanskarkumar109@users.noreply.github.com>
2024-05-05 20:11:51 +0530

    Add files via upload

---
+++ guesssthenum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class guesssthenum {
+    int num;
+
+    public void input(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your number");
+        num=sc.nextInt();
+
+    }
+    
+    
+}
